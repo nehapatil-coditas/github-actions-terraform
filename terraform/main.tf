58c60dbd7c0d4cfd036a411304889d69fd9072ef
nehapatil-coditas <neha.patil@coditas.com>
2024-07-16 12:01:56 +0530

    Changed ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ provider "aws" {
 }
  
 resource "aws_instance" "test_instance" {
- ami           = "ami-830c94e3"
+ ami           = "ami-01fccab91b456acc2"
  instance_type = "t2.nano"
  tags = {
    Name = "test_instance"
